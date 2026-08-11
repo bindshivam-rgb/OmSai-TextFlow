51764a658615136396df408fd155d089cc7c0915
Shivam <bindshivam111@gmail.com>
2026-08-12 02:26:08 +0530

    Fix email SMTP configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,23 +1,59 @@
-const nodemailer = require("nodemailer");
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const sendEmail = async (options) => {
- await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    const { data, error } = await resend.emails.send({
+        from: "OmSai-TextFlow <onboarding@resend.dev>",
+        to: [options.email],
+        subject: options.subject,
+        text: options.message
+    });
 
-    to: options.email,
+    if (error) {
+        throw new Error(error.message);
+    }
 
-    subject: options.subject,
+    return data;
+};
 
-    text: options.message
-});
-}
 module.exports = sendEmail;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const nodemailer = require("nodemailer");
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     family: 4,
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// });
+// const sendEmail = async (options) => {
+//  await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+
+//     to: options.email,
+
+//     subject: options.subject,
+
+//     text: options.message
+// });
+// }
+// module.exports = sendEmail;
